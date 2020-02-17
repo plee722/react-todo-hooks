eb028d28ab12ef13b2013de2c2d8de1c22bd403a
Peter Lee <pil206@nyu.edu>
2020-02-17 18:02:47 -0500

    created Todo component

---
+++ src/components/Todo.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import './Todo.css'
+
+function Task({ task }) {
+  return (
+    <div className="task" style={{ textDecoration: task.completed ? "line-through" : "" }} >
+      {task.title}
+    </div>
+  )
+}
+
+function Todo() {
+  const [tasks, setTasks] = useState([
+    {
+      title: "Grab some pizza",
+      completed: true
+    },
+    {
+      title: "Do your workout",
+      completed: true
+    },
+    {
+      title: "Hangout with friends",
+      completed: false
+    }
+  ])
+  return (
+    <div className="todo-container">
+      <div className="header">ToDo Items</div>
+      <div className="tasks">
+        {tasks.map((task, index) => (
+          <Task task={task} index={index} key={index} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Todo
