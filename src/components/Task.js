486c44bf16553bf4e85a4fa75bad075bfe02cb40
Peter Lee <pil206@nyu.edu>
2020-02-17 18:05:34 -0500

    separated Task into own component

---
+++ src/components/Task.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Task({ task }) {
+  return (
+    <div className="task" style={{ textDecoration: task.completed ? "line-through" : "" }} >
+      {task.title}
+    </div>
+  )
+}
+
+export default Task
